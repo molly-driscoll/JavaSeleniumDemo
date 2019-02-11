83dc9dbfdbbdbaccacf7d2be8de765e69a9ce9aa
iflanagan <ianianf@yahoo.com>
2019-02-11 13:42:19 -0600

    changed the way the functions are called.

--- src/test/java/com/DemoTest/Tests/WebDemoTest.java
+++ src/test/java/com/DemoTest/Tests/WebDemoTest.java
@@ -19,34 +19,80 @@ public class WebDemoTest extends TestBase {
      *
      * @throws InvalidElementStateException
      */
-    @Test(dataProvider = "hardCodedBrowsers")
-    public void LoginTestSuite(String browser, String version, String os, Method method)
-            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
-    	
-    	Boolean value = true;
 
-        //create webdriver session
-        this.createDriver(browser, version, os, method.getName());
-        WebDriver driver = this.getWebDriver();
+	 @Test(dataProvider = "hardCodedBrowsers")
+	    public void Login(String browser, String version, String os, Method method)
+	            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+	    	
+	    	Boolean value = true;
 
-        this.annotate("Visiting sauce labs demo page...");
-        WebDemoPage page = WebDemoPage.visitPage(driver);
-        
-        this.annotate("Login Test..");
-        Boolean returnvalue = page.Login("standard_user", "secret_sauce");
-        
-        this.annotate("Login Locked out user Test..");
-        Boolean returnedvalue = page.LoginLockedoutUser("locked_out_user", "secret_sauce");
-        
-        // 
-        this.annotate("Login Performance Glitch user Test..");
-        Boolean returnedvalueglitch = page.LoginPerfGlitchUser("performance_glitch_user", "secret_sauce");
-        
+	        //create webdriver session
+	        this.createDriver(browser, version, os, method.getName());
+	        WebDriver driver = this.getWebDriver();
+
+	        this.annotate("Visiting sauce labs demo page...");
+	        WebDemoPage page = WebDemoPage.visitPage(driver);
+	        
+	        this.annotate("Login Test..");
+	        Boolean returnvalue = page.Login("standard_user", "secret_sauce");
+	        
+	        this.annotate("Asserting the test: Login Test: result");
+	        Assert.assertEquals(value,returnvalue);
+
+	             
+	    }
 
 
-        this.annotate("Asserting that we are on a new page...");
-        Assert.assertEquals(value,returnvalue);
-    }
+	 @Test(dataProvider = "hardCodedBrowsers")
+	    public void LoginLockedoutUser(String browser, String version, String os, Method method)
+	            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+	    	
+	    	Boolean value = true;
+
+	        //create webdriver session
+	        this.createDriver(browser, version, os, method.getName());
+	        WebDriver driver = this.getWebDriver();
+
+	        this.annotate("Visiting sauce labs demo page...");
+	        WebDemoPage page = WebDemoPage.visitPage(driver);
+	        
+	       
+	        
+	        this.annotate("Login Locked out user Test..");
+	        Boolean returnvalue = page.LoginLockedoutUser("locked_out_user", "secret_sauce");
+	        
+	        this.annotate("Asserting the test: Login Locked out user: result");
+	        Assert.assertEquals(value,returnvalue);
+
+	        
+	     
+	    }
+
+	@Test(dataProvider = "hardCodedBrowsers")
+	    public void LoginPerfGlitchUser(String browser, String version, String os, Method method)
+	            throws MalformedURLException, InvalidElementStateException, UnexpectedException {
+	    	
+	    	Boolean value = true;
+
+	        //create webdriver session
+	        this.createDriver(browser, version, os, method.getName());
+	        WebDriver driver = this.getWebDriver();
+
+	        this.annotate("Visiting sauce labs demo page...");
+	        WebDemoPage page = WebDemoPage.visitPage(driver);
+	        
+	       
+	         // 
+	        this.annotate("Login Performance Glitch user Test..");
+	        Boolean returnedvalueglitch = page.LoginPerfGlitchUser("performance_glitch_user", "secret_sauce");
+	        
+	        this.annotate("Asserting the test: Login Performance Glitch user Test: result");
+	        Assert.assertEquals(value,returnedvalueglitch);
+	        
+	             
+	    }
+
+
 
 
 }
